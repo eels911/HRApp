3af2b1f00d2d970fbe8fdd4be20de54b6e664da8
Sabina <rigby917@gmail.com>
2022-01-13 11:03:15 +0300

    regex telnumber

--- app/src/main/java/com/example/android/hrapp/FragmentContactDetails.kt
+++ app/src/main/java/com/example/android/hrapp/FragmentContactDetails.kt
@@ -5,19 +5,26 @@ import android.view.View
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.android.hrapp.domain.WorkerDataSource
-import com.example.android.hrapp.models.Worker
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
-class FragmentContactDetails: Fragment(R.layout.fragment_contact_details) {
-    private val name: TextView by lazy { requireView().findViewById<TextView>(R.id.name) }
+class FragmentContactDetails : Fragment(R.layout.fragment_contact_details) {
+    private val profileNumber: TextView by lazy { requireView().findViewById<TextView>(R.id.tv_profile_number) }
+    private val profileFI: TextView by lazy { requireView().findViewById<TextView>(R.id.tv_fiodata) }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val id = arguments?.getInt(TelephoneBookFragment.WORKER_ID) ?: 0
-        name.setText(loadWorkerInfo(id).toString())
+
+        val data = loadWorkerInfo(id).toString()
+//        if (name.contains("telephone=",false))
+        val pattern: Pattern = Pattern.compile("(\\+*)\\d{11}")
+        val matcher: Matcher = pattern.matcher(data)
+        if (matcher.find())
+            profileNumber.setText(matcher.group(0))
 
     }
 
     private fun loadWorkerInfo(id: Int): Any? {
-       return WorkerDataSource().getWorkersById(id)
-
+        return WorkerDataSource().getWorkersById(id)
     }
 }
